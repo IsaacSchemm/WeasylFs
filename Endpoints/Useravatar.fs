1f5229a950a0bf0ef8f114be3f4f49177e2897e1
Isaac Schemm <isaacschemm@gmail.com>
2020-01-14 10:35:58 -0600

    Add whoami and useravatar

---
+++ Endpoints/Useravatar.fs
@@ -0,0 +1,27 @@
+﻿namespace WeasylFs.Endpoints
+
+open WeasylFs
+open System.IO
+open FSharp.Json
+open System
+
+module UserAvatar =
+    type Response = {
+        avatar: string
+    }
+
+    let AsyncExecute credentials username = async {
+        let req =
+            username
+            |> Uri.EscapeUriString
+            |> sprintf "/api/useravatar?username=%s"
+            |> Shared.createRequest credentials
+        let! resp = req.AsyncGetResponse()
+        use sr = new StreamReader(resp.GetResponseStream())
+        let! json = sr.ReadToEndAsync() |> Async.AwaitTask
+        return Json.deserialize<Response> json
+    }
+
+    let ExecuteAsync credentials username =
+        AsyncExecute credentials username
+        |> Async.StartAsTask
---
+++ Endpoints/Whoami.fs
@@ -0,0 +1,23 @@
+﻿namespace WeasylFs.Endpoints
+
+open WeasylFs
+open System.IO
+open FSharp.Json
+
+module Whoami =
+    type Response = {
+        login: string
+        userid: int
+    }
+
+    let AsyncExecute credentials = async {
+        let req = Shared.createRequest credentials "/api/whoami"
+        let! resp = req.AsyncGetResponse()
+        use sr = new StreamReader(resp.GetResponseStream())
+        let! json = sr.ReadToEndAsync() |> Async.AwaitTask
+        return Json.deserialize<Response> json
+    }
+
+    let ExecuteAsync credentials =
+        AsyncExecute credentials
+        |> Async.StartAsTask
