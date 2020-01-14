4edff9413227fcbbc4f60ad5bfde94201d6b09f0
Isaac Schemm <isaacschemm@gmail.com>
2020-01-14 10:24:47 -0600

    Add Task wrapper and response type

--- Endpoints/Version.fs
+++ Endpoints/Version.fs
@@ -2,12 +2,21 @@
 
 open WeasylFs
 open System.IO
+open FSharp.Json
 
 module Version =
+    type Response = {
+        short_sha: string
+    }
+
     let AsyncExecute credentials = async {
         let req = Shared.createRequest credentials "/api/version"
         let! resp = req.AsyncGetResponse()
         use sr = new StreamReader(resp.GetResponseStream())
         let! json = sr.ReadToEndAsync() |> Async.AwaitTask
-        return json
+        return Json.deserialize<Response> json
     }
+
+    let ExecuteAsync credentials =
+        AsyncExecute credentials
+        |> Async.StartAsTask
