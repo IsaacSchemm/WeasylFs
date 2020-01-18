20441d36c4d171a6df0a8c0020794f7418fb65d1
Isaac Schemm <isaacschemm@gmail.com>
2020-01-17 21:09:47 -0500

    Add /api/submissions/frontpage and /api/submissions/(submitid)/view

---
+++ Endpoints/FrontPage.fs
@@ -0,0 +1,31 @@
+﻿namespace WeasylFs.Endpoints
+
+open WeasylFs
+open System.IO
+open FSharp.Json
+open System
+
+module FrontPage =
+    type Request() =
+        member val Since = Nullable<DateTimeOffset>() with get, set
+        member val Count = Nullable<int>() with get, set
+
+    let AsyncExecute credentials (parameters: Request) = async {
+        let query = seq {
+            if parameters.Since.HasValue then
+                yield ("since", parameters.Since.Value.ToString("o"))
+            if parameters.Count.HasValue then
+                yield ("count", parameters.Count.Value.ToString())
+        }
+        let qs = Shared.buildQueryString query
+        let url = sprintf "/api/submissions/frontpage?%s" qs 
+        let req = Shared.createRequest credentials url
+        let! resp = req.AsyncGetResponse()
+        use sr = new StreamReader(resp.GetResponseStream())
+        let! json = sr.ReadToEndAsync() |> Async.AwaitTask
+        return Json.deserialize<Submission list> json
+    }
+
+    let ExecuteAsync credentials parameters =
+        AsyncExecute credentials parameters
+        |> Async.StartAsTask
---
+++ Endpoints/ViewSubmission.fs
@@ -0,0 +1,57 @@
+﻿namespace WeasylFs.Endpoints
+
+open WeasylFs
+open System.IO
+open FSharp.Json
+open System
+
+module ViewSubmission =
+    type Request() =
+        member val Anyway = false with get, set
+        member val IncrementViews = false with get, set
+
+    type Response = {
+        rating: string
+        link: string
+        owner: string
+        owner_login: string
+        submitid: int
+        title: string
+        media: Map<string, Media list>
+        posted_at: DateTimeOffset
+        subtype: string
+        ``type``: string
+        
+        embedlink: string option
+        description: string
+        tags: string list
+        comments: int
+        owner_media: Map<string, Media list>
+        favorited: bool
+        folder_name: string option
+        favorites: int
+        folderid: int option
+        views: int
+        friends_only: bool
+    }
+
+    let AsyncExecute credentials (parameters: Request) submitid = async {
+        let query = seq {
+            if parameters.Anyway then
+                yield ("anyway", "true")
+            if parameters.IncrementViews then
+                yield ("increment_views", "true")
+        }
+        let qs = Shared.buildQueryString query
+        let url = sprintf "/api/submissions/%d/view?%s" submitid qs 
+        let req = Shared.createRequest credentials url
+        let! resp = req.AsyncGetResponse()
+        use sr = new StreamReader(resp.GetResponseStream())
+        let! json = sr.ReadToEndAsync() |> Async.AwaitTask
+        System.IO.File.WriteAllText("C:/Users/admin/Desktop/out.json", json)
+        return Json.deserialize<Response> json
+    }
+
+    let ExecuteAsync credentials parameters submitid =
+        AsyncExecute credentials parameters submitid
+        |> Async.StartAsTask
