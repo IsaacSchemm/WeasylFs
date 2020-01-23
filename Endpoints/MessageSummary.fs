cde56c280f04d8cbd5a8fb9df49eb13fced12737
Isaac Schemm <isaacschemm@gmail.com>
2020-01-23 10:56:56 -0500

    Add MessageSummary

---
+++ Endpoints/MessageSummary.fs
@@ -0,0 +1,21 @@
+﻿namespace WeasylFs.Endpoints
+
+open WeasylFs
+
+module MessageSummary =
+    type Response = {
+        comments: int
+        journals: int
+        notifications: int
+        submissions: int
+        unread_notes: int
+    }
+
+    let AsyncExecute credentials =
+        "api/messages/summary"
+        |> Util.CreateRequest credentials
+        |> Util.AsyncReadJson<Response>
+
+    let ExecuteAsync credentials =
+        AsyncExecute credentials
+        |> Async.StartAsTask
